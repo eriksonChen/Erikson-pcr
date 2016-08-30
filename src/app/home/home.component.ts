fd65fb34f8bfe8de502ee153193d8c49d96f165b
Erikson chen <botan3951@gmail.com>
2016-08-30 13:52:17 +0800

    update file

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,18 +14,21 @@ export class HomeComponent implements OnInit {
   	window.scrollTo(0,0);
   	TweenMax.set('.bgimg', {x:'-52%', y:'-49%'});
   	TweenMax.set('.text-box', {x:'-50%', y:'-50%'});
+    $('.bgimg, .text-box').css('display','none');
 
-  	$('.wrap').fadeIn(()=>{
-
-  		this.anim();
-  	});
+    setTimeout(()=>{
+      $('.wrap').fadeIn(()=>{
+        this.anim();
+      });
+    },10);
+  	
   	
   }
 
   anim(){
-  	$('.bgimg, .text-box').css('display','block');
-  	TweenMax.from('.bgimg', 0.8, {delay:0.2, alpha:0, scaleX:0.4, scaleY:0.4, rotation:60, ease:Expo.easeOut });
-  	TweenMax.from('.text-box', 0.6, {delay:0.6, alpha:0, y:-30, scaleY:1.5, scaleX:1.5, ease:Expo.easeOut});
+    $('.bgimg, .text-box').css('display','block');
+  	TweenMax.from('.bgimg', 1, {delay:0.3, alpha:0, scaleX:0.4, scaleY:0.4, rotation:50, ease:Expo.easeOut });
+  	TweenMax.from('.text-box', 0.6, {delay:0.6, alpha:0, y:-30, scaleY:1.5, scaleX:1.5, ease:Cubic.easeOut});
   }
 
 }
--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -13,6 +13,13 @@ export class ResumeComponent implements OnInit {
   	ngOnInit() {
   		// this.aboutService.changeBar(false);
   		window.scrollTo(0,0);
+  		$('.wrap').css('display','none').fadeIn(()=>{
+  			
+  		});
+  		$('.txt-box').each((index, ele)=>{
+          TweenMax.from(ele, 0.6, {delay:(index+1.5)*0.2, alpha:0, y:70, ease:Cubic.easeOut});
+        });
+  		
   	}
 
 }
