142bdbe1e87cc08aa19c46c88c86bf16d537ae8e
Erikson Chen <botan3951@gmail.com>
2016-08-31 23:10:05 +0800

    update src file

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,41 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, RequestOptions, Headers, Response } from '@angular/http';
+import { FormGroup, FormControl, FormBuilder, REACTIVE_FORM_DIRECTIVES} from '@angular/forms';
+
 
 @Component({
   // selector: 'app-contact',
   templateUrl: 'contact.component.html',
-  styleUrls: ['../../sass/contact.scss']
+  styleUrls: ['../../sass/contact.scss'],
+  directives: [REACTIVE_FORM_DIRECTIVES]
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  form={};
 
-  ngOnInit() {
-
-  	var $contactForm = $('#contact-form');
-    $contactForm.submit(function(e) {
-        console.log('submit');
-        e.preventDefault();
-        $.ajax({
-            url: '//formspree.io/botan3951@gmail.com',
-            method: 'POST',
-            data: $(this).serialize(),
-            dataType: 'json',
-            beforeSend: function() {
-                // $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
-            },
-            success: function(data) {
-                /*$contactForm.find('.alert--loading').hide();
-                $contactForm.append('<div class="alert alert--success">Message sent!</div>');*/
-                alert('Message sent!');
-            },
-            error: function(err) {
-               alert(err);
-            }
-        });
-    });
-
+  constructor(private http:Http) {
+    this.form={
+      name:'',
+      _subject:'',
+      email: '',
+      Message: '',
+      _next:'http://e3pcr.com'
+    }
   }
 
+  ngOnInit() {
+    $('.hiddendiv').css("display","none");
 
+    $('.wrap').css('display', 'none').fadeIn();
+  }
+
+  onSubmit(){
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+    var data = $.param(this.form);
+    // var data = JSON.stringify(this.form);
+    // console.log($('#contact-form').serialize());
+
+    this.http.post('http://formspree.io/botan3951@gmail.com', data, {headers: headers})
+      // .map((res:Response) => res.json())
+      .subscribe(
+        err => this.logError(err),
+        () => this.clearForm()
+      );
+  }
+
+  logError(err) {
+    console.error('There was an error: ' + err);
+  }
+
+  clearForm(){
+    alert('寄送成功！');
+
+    this.form={
+      name:'',
+      _subject:'',
+      email: '',
+      Message: '',
+      _next:'http://e3pcr.com'
+    }; 
+  }
 
 }
