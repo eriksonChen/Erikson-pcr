a2494953baabbf63bb22d7bae8beda6a83ba826d
Erikson Chen <botan3951@gmail.com>
2016-09-09 19:58:11 +0800

    change file

--- src/app/hobbies/hobbies.component.ts
+++ src/app/hobbies/hobbies.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MdButton } from '@angular2-material/button';
 
 @Component({
   selector: 'app-hobbies',
   templateUrl: 'hobbies.component.html',
-  styleUrls: ['../../sass/hobbies.scss']
+  styleUrls: ['../../sass/hobbies.scss'],
+  directives: [MdButton]
 })
 export class HobbiesComponent implements OnInit {
 
