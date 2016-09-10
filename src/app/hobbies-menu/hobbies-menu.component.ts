37f8ec19d12aedb757602ea5cc0172f40decc0c9
Erikson chen <botan3951@gmail.com>
2016-09-10 20:20:31 +0800

    fixed file

--- src/app/hobbies-menu/hobbies-menu.component.ts
+++ src/app/hobbies-menu/hobbies-menu.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hobbies-menu',
   templateUrl: 'hobbies-menu.component.html',
   styleUrls:['./hobbies-menu.component.scss']
 })
-export class HobbiesMenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class HobbiesMenuComponent {
 
 }
