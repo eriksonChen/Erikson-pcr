78059bef7024506517dda57852cb7c0aeec2abbd
Erikson chen <botan3951@gmail.com>
2016-10-04 22:12:47 +0800

    add gaclick

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,30 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['../sass/style.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   constructor() { }
-  ngOnInit(){
+  ngOnInit() {
     gapage('erikson_index');
+    if (this.paVaule('p') == '4') {
+      gaclick('--104--');
+    }
+  }
+
+  paVaule(par) {
+    var PA = location.search.split("?");
+    var pas = (PA[1]) ? PA[1] : "";
+
+    if (pas == '' || par == '') {
+      return "";
+    }
+    var pasArr = pas.split("&");
+    for (var i = 0; i < pasArr.length; i++) {
+      var nowVal = pasArr[i].split("=");
+      if (nowVal[0] == par && nowVal[1]) {
+        return decodeURI(pasArr[i].split("=")[1]);
+      }
+    }
+    return "";
   }
 
 }
